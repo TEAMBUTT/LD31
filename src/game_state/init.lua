b48fa7151bb512762de1ddd74ce08a0e9637d118
Jared Norman <jared@freerunningtechnologies.com>
2014-04-02 21:46:52 -0700

    Throw some errors
    Change-Id: If140a11b8bf0b649785eea450fb23b24abb57fb8

--- src/game_state/init.lua
+++ src/game_state/init.lua
@@ -2,6 +2,11 @@
 local GameState = class 'GameState'
 
 function GameState:update()
+  error 'must be overridden'
+end
+
+function GameState:draw()
+  error 'must be overridden'
 end
 
 return GameState
