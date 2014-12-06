3d723661dd140fb55998bf6c12dc04893675be16
John Hawthorn <john@freerunningtechnologies.com>
2014-12-06 13:38:52 -0800

    Hacky player animations

--- src/player.lua
+++ src/player.lua
@@ -11,12 +11,16 @@ knight.module("Game")
     0, 3
   }, function(x) return x * 8 end)
 
+  local image = love.graphics.newImage("player.png")
+
   function Player:initialize()
     Entity.initialize(self)
     self.body = love.physics.newBody(world, 640/3, 640/3, "dynamic")
     self.body:setFixedRotation(true)
     self.shape = love.physics.newPolygonShape(unpack(shape_coordinates))
     self.fixture = love.physics.newFixture(self.body, self.shape, 1)
+
+    self.anim = newAnimation(image, 32, 32, 0.1, 0)
 
     Entity.on_destroy(self, function() self.body:destroy() end)
 
@@ -29,11 +33,16 @@ knight.module("Game")
   end
 
   function Player:update(dt)
+    self.anim:update(dt)
   end
 
   function Player:draw(e)
     love.graphics.setColor(unpack(palette.violet))
     love.graphics.polygon('fill', self.body:getWorldPoints(self.shape:getPoints()))
+
+    local x, y = self.body:getWorldCenter()
+
+    self.anim:draw(x-16, y-16)
   end
 
   return Player:new()
