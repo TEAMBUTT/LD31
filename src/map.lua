cca67458abe86adb352b24f9389f63bd2af6f2d9
Jared Norman <jared@freerunningtechnologies.com>
2014-12-06 16:25:53 -0800

    Remove references to hardcoded screen height
    Change-Id: I1c4fe0733842ba70f661ade984ba0db279567f08

--- src/map.lua
+++ src/map.lua
@@ -3,9 +3,11 @@ knight
 .component("map",
 {"event", "world", "palette", "Platform", "FloatyPlatform"},
 function(event, world, palette, Platform, FloatyPlatform)
-  local floor = Platform:new(0, 768-32, 1024, 32)
-  local left_wall = Platform:new(-100, 0, 100, 768)
-  local right_wall = Platform:new(1024, 0, 100, 768)
+  local height = love.graphics.getHeight()
+
+  local floor = Platform:new(0, height - 32, 1024, 32)
+  local left_wall = Platform:new(-100, 0, 100, height)
+  local right_wall = Platform:new(1024, 0, 100, height)
   local ceiling = Platform:new(0, -100, 1024, 100)
 
   FloatyPlatform:new( 6, 15)
