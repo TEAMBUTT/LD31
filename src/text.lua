ec1be2a78cdb694afa8fc826c5eac83249798650
John Hawthorn <john@freerunningtechnologies.com>
2014-12-07 13:58:04 -0800

    MOAR messages

--- src/text.lua
+++ src/text.lua
@@ -6,10 +6,20 @@ knight.module("Game")
     "123456789.,!?-+/():;%&`'*#=[]\"")
   font:setFilter('nearest', 'nearest', 1)
 
-  local top_text = "ZOMG!\nCollect all the broken screen pieces,\nyou noob!"
+  local messages = {
+    "The screen is broken! I can barely see!\n\nCOLLECT THE BROKEN SCREEN PIECES",
+    "I can see a bit better now.\n\nMOAR SCREEN PIECES"
+  }
+  local top_text = messages[1]
 
   event:on("piece_collected", function(collected, total)
-    top_text = "Wow! You got one!\n" .. (total - collected) .. " more to go!"
+    if messages[collected + 1] then
+      top_text = messages[collected + 1]
+    elseif total == collected then
+      top_text = "You win. I guess."
+    else
+      top_text = "Just " .. (total - collected) .. " more to go!"
+    end
   end)
 
   event:on("draw_overlay", function()
