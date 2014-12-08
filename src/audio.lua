f8cffd00924dd34dc21bbbd5929f2f8f3ca43404
John Hawthorn <john@freerunningtechnologies.com>
2014-12-07 17:01:46 -0800

    Add missing mp3 file

--- src/audio.lua
+++ src/audio.lua
@@ -2,8 +2,9 @@ knight.module("Game")
 .component("audio", {"event"}, function(event)
   local function play_music(filename)
     local music = love.audio.newSource("audio/" .. filename)
+    music:setLooping(true)
     music:play()
   end
 
-  event:on("load", function() play_music("music.wav") end)
+  event:on("load", function() play_music("DT3.mp3") end)
 end)
